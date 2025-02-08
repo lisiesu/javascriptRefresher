48f084dfb715599d05b1098cc6571e56bc60f54b
Aline Silva <lisie_su@yahoo.com.br>
2025-02-08 17:31:27 +0000

    Setting up postgres and prisma

--- notes-app/notes-backend/src/index.ts
+++ notes-app/notes-backend/src/index.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/notes", async (req, res) => {
-  res.json({ message: "Success!" });
+  const notes = await prisma.note.findMany();
+
+  res.json(notes);
 });
 
 app.listen(5000, () => {
